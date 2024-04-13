09711e965403e533dd15b570c3943148b0f4bcdf
Sparsh Gupta <sparshg@gmail.com>
2024-04-13 14:42:37 -0400

    Implement sha256 hashing class

---
+++ src/sha256.h
@@ -0,0 +1,25 @@
+#ifndef CELESTRIXBLOCKCHAIN_SHA256_H
+#define CELESTRIXBLOCKCHAIN_SHA256_H
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+class sha256 {
+private:
+    static const uint32_t K[];
+    static const uint32_t initialHashValues[];
+
+    static uint32_t rightRotate(uint32_t value, uint32_t n);
+    static uint32_t sigma0(uint32_t value);
+    static uint32_t sigma1(uint32_t value);
+    static uint32_t Sigma0(uint32_t value);
+    static uint32_t Sigma1(uint32_t value);
+    static std::vector<uint32_t> computeMessageSchedule(const std::vector<uint8_t>& block);
+
+public:
+    static std::string hash(std::string& message);
+};
+
+
+#endif //CELESTRIXBLOCKCHAIN_SHA256_H
