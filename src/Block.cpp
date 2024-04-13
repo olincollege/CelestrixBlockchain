ebf940711aa611a521bee3314d6fb2b09b0703ae
Sparsh Gupta <sparshg@gmail.com>
2024-04-13 18:04:44 -0400

    Add methods to Block (some placeholders)

--- src/Block.cpp
+++ src/Block.cpp
@@ -1,1 +1,87 @@
 #include "Block.h"
+#include "sha256.h"
+
+Block::Block(std::string previousHash, std::time_t timestamp, std::vector<Transaction> transactions)
+    : previousHash(previousHash), timestamp(timestamp), transactions(transactions) {
+    version = 1;
+    nonce = 0;
+    merkleRoot = calculateMerkleRoot();
+    blockHash = calculateBlockHash();
+}
+
+std::time_t Block::getTimestamp() const {
+    return std::time(nullptr);
+}
+
+std::string Block::getBlockHash() const {
+    return blockHash;
+}
+
+std::string Block::calculateBlockHash() const {
+    std::stringstream ss;
+    ss << version << previousHash << timestamp << merkleRoot << blockSignature << nonce << difficultyTarget;
+    std::string data = ss.str();
+    return sha256::hash(data);
+}
+
+void Block::mineBlock(int difficulty) {
+    merkleRoot = calculateMerkleRoot();
+    // Proof of Work - Mining
+    do {
+        nonce++;
+        blockHash = calculateBlockHash();
+    } while (blockHash.substr(0, static_cast<unsigned long>(difficulty)) != std::string(
+            static_cast<unsigned long>(difficulty), '0'));
+}
+
+std::string Block::getPreviousHash() const {
+    return previousHash;
+}
+
+std::vector<Transaction> Block::getTransactions() const {
+    return transactions;
+}
+
+int Block::getBlockHeight() const {
+    return 0;
+}
+
+bool Block::validateBlock() const {
+    return false;
+}
+
+int Block::getBlockSize() const {
+    return 0;
+}
+
+std::string Block::getBlockSignature() const {
+    return blockSignature;
+}
+
+std::vector<uint8_t> Block::serialize() const {
+    return std::vector<uint8_t>();
+}
+
+Block Block::deserialize(const std::vector<uint8_t> &serializedData) {
+}
+
+std::string Block::calculateMerkleRoot() const {
+    return "";
+}
+
+std::string Block::getMerkleRoot() const {
+    return merkleRoot;
+}
+
+int Block::getNonce() const {
+    return nonce;
+}
+
+void Block::setDifficulty(int difficulty) {
+    difficultyTarget = difficulty;
+}
+
+void Block::addTransaction(const Transaction &transaction) {
+    transactions.push_back(transaction);
+}
+
--- src/Block.h
+++ src/Block.h
@@ -5,22 +5,21 @@
 #include <ctime>
 #include <string>
 #include <vector>
+#include <sstream>
+#include <iostream>
 
 class Block {
 private:
+  // Block header
+  int version;
   std::string previousHash;
   std::string blockHash;
-  std::time_t timeStamp;
+  std::time_t timestamp;
   std::vector<Transaction> transactions;
-  // Block specifications
-  int version;
-  int height;
-  int size;
-  int weight;
   // Proof of Work members
   std::string merkleRoot;
   std::string blockSignature;
-  std::string nonce;
+  int nonce;
   int difficultyTarget;
 
 public:
@@ -28,12 +27,21 @@ public:
         std::vector<Transaction> transactions);
   std::string getBlockHash() const;
   std::string calculateBlockHash() const;
-  void mineBlock(int difficulty);
   std::string getPreviousHash() const;
   std::vector<Transaction> getTransactions() const;
   std::time_t getTimestamp() const;
-
-  // TODO: Add other methods and members
+  void mineBlock(int difficulty);
+  int getBlockHeight() const;
+  int getBlockSize() const;
+  bool validateBlock() const;
+  std::string getBlockSignature() const;
+  std::vector<uint8_t> serialize() const;
+  static Block deserialize(const std::vector<uint8_t>& serializedData);
+  std::string calculateMerkleRoot() const;
+  std::string getMerkleRoot() const;
+  int getNonce() const;
+  void setDifficulty(int difficulty);
+  void addTransaction(const Transaction &transaction);
 };
 
 #endif // CELESTRIXBLOCKCHAIN_BLOCK_H
