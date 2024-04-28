6d48ee33b7f0c0b0853b5ef1305e2fc10deb2aa9
Sparsh Gupta <sparshg@gmail.com>
2024-04-28 16:12:33 -0400

    Add documentation to code

--- src/Block.h
+++ src/Block.h
@@ -13,6 +13,9 @@
 #include <utility>
 #include <vector>
 
+/**
+ * @brief Class representing a block in the blockchain.
+ */
 class Block {
 private:
   // Block header
@@ -28,23 +31,110 @@ private:
   int difficultyTarget{};
 
 public:
+  /**
+   * @brief Constructor for Block class.
+   * @param index Index of the block.
+   * @param version Version of the block.
+   * @param previousHash Hash of the previous block.
+   * @param timestamp Timestamp of when the block was created.
+   * @param transactions List of transactions in the block.
+   * @param nonce Nonce used in mining the block.
+   * @param difficultyTarget Difficulty target for mining.
+   */
   Block(int index, int version, std::vector<std::byte> previousHash,
         std::time_t timestamp, std::vector<Transaction> transactions, int nonce,
         int difficultyTarget);
+
+  /**
+   * @brief Gets the hash of the block.
+   * @return The hash of the block.
+   */
   [[nodiscard]] std::vector<std::byte> getBlockHash() const;
+
+  /**
+   * @brief Gets the hash of the previous block.
+   * @return The hash of the previous block.
+   */
   [[nodiscard]] std::vector<std::byte> getPreviousHash() const;
+
+  /**
+   * @brief Gets the list of transactions in the block.
+   * @return The list of transactions in the block.
+   */
   [[nodiscard]] std::vector<Transaction> getTransactions() const;
+
+  /**
+   * @brief Calculates the timestamp of the current time.
+   * @return Timestamp of the current time.
+   */
   static std::time_t getTimestamp();
+
+  /**
+   * @brief Gets the index of the block.
+   * @return The index of the block.
+   */
   [[nodiscard]] int getIndex() const;
+
+  /**
+   * @brief Gets the version of the block.
+   * @return The version of the block.
+   */
   [[nodiscard]] int getVersion() const;
+
+  /**
+   * @brief Gets the difficulty target for mining.
+   * @return The difficulty target for mining.
+   */
   [[nodiscard]] int getDifficulty() const;
+
+  /**
+   * @brief Gets the size of the block.
+   * @return The size of the block.
+   */
   [[maybe_unused]] [[nodiscard]] int getBlockSize() const;
+
+  /**
+   * @brief Gets the nonce used in mining the block.
+   * @return The nonce used in mining the block.
+   */
   [[nodiscard]] int getNonce() const;
+
+  /**
+   * @brief Calculates the hash of the block.
+   * @return The calculated hash of the block.
+   */
   [[nodiscard]] std::vector<std::byte> calculateBlockHash() const;
+
+  /**
+   * @brief Mines the block with a given difficulty target.
+   * @param difficulty Difficulty target for mining.
+   */
   void mineBlock(int difficulty);
+
+  /**
+   * @brief Adds a transaction to the block.
+   * @param transaction The transaction to be added.
+   */
   void addTransaction(const Transaction &transaction);
+
+  /**
+   * @brief Signs the block using the provided private key.
+   * @param privateKey The private key used for signing.
+   * @return True if signing is successful, false otherwise.
+   */
   bool signBlock(EVP_PKEY *privateKey);
+
+  /**
+   * @brief Verifies the signature of the block using the provided public key.
+   * @param publicKey The public key used for signature verification.
+   * @return True if signature verification is successful, false otherwise.
+   */
   [[nodiscard]] bool verifyBlockSignature(EVP_PKEY *publicKey) const;
+
+  /**
+   * @brief Generates a pair of EVP key (private and public key).
+   * @return A pair of EVP key (private and public key).
+   */
   static std::pair<EVP_PKEY *, EVP_PKEY *> generateEVPKeyPair();
 };
 
--- src/Blockchain.h
+++ src/Blockchain.h
@@ -4,6 +4,9 @@
 #include "Block.h"
 #include <iostream>
 
+/**
+ * @brief Class representing a blockchain.
+ */
 class Blockchain {
 private:
   std::vector<Block> chain;
@@ -11,10 +14,36 @@ private:
   mutable std::vector<Transaction> pendingTransactions;
 
 public:
+  /**
+   * @brief Constructs a new Blockchain object with the specified difficulty.
+   * @param difficulty The difficulty level for mining blocks in the blockchain.
+   */
   explicit Blockchain(int difficulty);
+
+  /**
+   * @brief Adds a block to the blockchain after mining it.
+   * @param block The block to be added to the blockchain.
+   */
   void addBlock(const Block &block);
+
+  /**
+   * @brief Retrieves a block from the blockchain by its index.
+   * @param index The index of the block to retrieve.
+   * @return The block at the specified index.
+   * @throws std::out_of_range if the index is out of bounds.
+   */
   Block getBlock(int index) const;
+
+  /**
+   * @brief Checks if the blockchain is valid by verifying the integrity of each
+   * block.
+   * @return true if the blockchain is valid, false otherwise.
+   */
   bool isChainValid() const;
+
+  /**
+   * @brief Prints the entire blockchain to the standard output.
+   */
   void printBlockchain() const;
 };
 
